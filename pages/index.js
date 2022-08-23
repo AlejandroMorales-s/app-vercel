fd7d1c3eb6a13ef60428a97db5573316846af07d
Alejandro <alejandrom.am33@gmail.com>
2022-08-22 22:40:08 -0500

    add: button to see all the pokemons

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hola mundo</h1>
+      <Link href={"/pokemon"}>
+        <h1>Ver todos los pokemon</h1>
+      </Link>
     </div>
   )
 }
